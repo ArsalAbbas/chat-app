bfb4d81669d9d6ba8723ce29686960e1f2d948aa
Syed Arsal Abbas <syedarsal.abbas2002@gmail.com>
2023-10-07 17:53:34 +0530

    added env file

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,21 +1,20 @@
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore'
-import 'firebase/compat/auth'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
-firebase.initializeApp(
-    {
-        apiKey: "AIzaSyDidkr9MYYQCnCymWmBXfs9P3zY6XCyJsA",
-        authDomain: "chat-app-37dc4.firebaseapp.com",
-        projectId: "chat-app-37dc4",
-        storageBucket: "chat-app-37dc4.appspot.com",
-        messagingSenderId: "742119306474",
-        appId: "1:742119306474:web:a4f4e05adf4d9b3e74d393",
-        measurementId: "G-ZJZX4LQ9FV"
-    })
-    const auth=firebase.auth();
-    const firestore=firebase.firestore();
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+});
 
-    const API={fbAuth: auth, fbStore: firestore}
-    
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+const API = { fbAuth: auth, fbStore: firestore };
 
 export default API;
